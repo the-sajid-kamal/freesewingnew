f42e94923b1bb889b02ef887cb4898727c0ee4ec
Jonathan Haas <haasjona@gmail.com>
2025-04-20 19:12:23 +0200

    Fix SampleMeasurementButton

--- packages/react/components/Editor/components/menus/TestMenu.mjs
+++ packages/react/components/Editor/components/menus/TestMenu.mjs
@@ -97,7 +97,7 @@ const SampleMeasurementButton = ({ name, i18n, update }) => (
       'tw:daisy-btn tw:daisy-btn-outline tw:daisy-btn-sm tw:mx-2 ' +
       'tw:daisy-btn-secondary tw:flex tw:flex-row tw:items-center tw:justify-between'
     }
-    onClick={() => update.settings('sample', { type: 'option', option: name })}
+    onClick={() => update.settings('sample', { type: 'measurement', measurement: name })}
   >
     <BeakerIcon className="tw:w-5 tw:h-5" />
     <span>{measurementsTranslations[name]}</span>
