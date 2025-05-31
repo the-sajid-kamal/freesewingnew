46176be32558f84ab8c7f607f4b32334fd20865a
Benjamin Fan <ben-git@swinglonga.com>
2025-05-30 17:52:15 -0700

    fix(react,css): Omit extra whitespace after last paragraph in Flag

--- packages/react/components/Mini/index.mjs
+++ packages/react/components/Mini/index.mjs
@@ -14,7 +14,7 @@ export const MiniTip = ({ children }) => (
     <div className="tw:bg-success tw:text-success-content tw:p-1 tw:rounded-l tw:flex tw:flex-row tw:items-center">
       <TipIcon className="tw:w-6 tw:h-6 tw:text-success-content" />
     </div>
-    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-success/10 tw:grow tw:rounded-r">
+    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-success/10 tw:grow tw:rounded-r mini">
       {children}
     </div>
   </div>
@@ -33,7 +33,7 @@ export const MiniNote = ({ children }) => (
     <div className="tw:bg-info tw:text-info-content tw:p-1 tw:rounded-l tw:flex tw:flex-row tw:items-center">
       <ChatIcon className="tw:w-6 tw:h-6 tw:text-info-content" />
     </div>
-    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-info/10 tw:grow tw:rounded-r">
+    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-info/10 tw:grow tw:rounded-r mini">
       {children}
     </div>
   </div>
@@ -52,7 +52,7 @@ export const MiniWarning = ({ children }) => (
     <div className="tw:bg-warning tw:text-warning-content tw:p-1 tw:rounded-l tw:flex tw:flex-row tw:items-center">
       <WarningIcon className="tw:w-6 tw:h-6 tw:text-warning-content" />
     </div>
-    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-warning/10 tw:grow tw:rounded-r">
+    <div className="tw:p-1 tw:px-2 tw:text-sm tw:font-medium tw:bg-warning/10 tw:grow tw:rounded-r mini">
       {children}
     </div>
   </div>
